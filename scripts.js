3571ddc7709ca8fd5a1d6b9ccdbbe2a2f4969fdf
hugovalentimr <hugovalentimvr@gmail.com>
2024-10-23 00:35:48 -0300

    Second Change - I changed some settings

--- scripts.js
+++ scripts.js
@@ -3,8 +3,21 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-origem").value)
     const max = Math.floor(document.querySelector(".input-final").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    if(min >= max){
+        alert("O valor mínimo tem que ser menor que o valor máximo!")
+    } else {        
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
 
-    alert(result)
+        alert(result)
+    }
 }
 
+
+// const min = 0
+// const max = 100
+
+// console.log(min < max)
+
+// if {
+//     const 
+// }
